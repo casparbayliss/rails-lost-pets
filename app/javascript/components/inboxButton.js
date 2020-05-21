339a3311a56c7b317b887fcef0711e977a670057
Caspar Bayliss <casparbayliss@gmail.com>
2020-05-21 10:28:34 +0100

    got the inbox to collapse

---
+++ app/javascript/components/inboxButton.js
@@ -0,0 +1,75 @@
+console.log("app/javascript/components/inboxButton.js is working");
+
+const inboxCross = document.querySelector("#inbox-cross");
+const conversationsList = document.querySelector("#conversations-list")
+const inboxWord = document.querySelector("#inbox-word");
+const tempBox = document.querySelector("#temp-box");
+console.log(document.cookie);
+// This is to get rid of the Google Adwords that has inserted
+// itself into my cookies
+// Split it into an array
+// var allCookies = document.cookie;
+
+// Set the regex as the beginning of my custom cookies
+const regex = /^displayed/
+const removeGoogle = (cookieArray) => {
+	var j = 0;
+	while (j < cookieArray.length) {
+		if (regex.test(cookieArray[j]) == false ) {
+			cookieArray.splice(j, 1);
+		} else {
+			j++;
+		};
+	};
+	console.log("line 25: " + cookieArray);
+	return cookieArray;
+};
+
+const hiddenInboxBoolean = () => {
+	console.log("hiddenInboxBoolean has been activated");
+	var cookieArray = document.cookie.split("; ");
+	console.log("line 32: " + cookieArray);
+	removeGoogle(cookieArray);
+	console.log("line 34: " + cookieArray);
+	if (cookieArray[0] == "displayed=yes") {
+		// document.cookie = "displayed=";
+		conversationsList.style.display = "block";
+		inboxCross.style.display = "block";
+		console.log("line37: " + cookieArray)
+		clickInboxCross(cookieArray);
+	} else if (cookieArray[0] == "displayed=no") {
+		console.log("line40: " + cookieArray);
+		conversationsList.style.display = "none";
+		inboxCross.style.display = "none";
+		clickInboxTitle(cookieArray);
+	}
+};
+
+const clickInboxTitle = (cookieArray) => {
+	console.log("Inbox title ready to be clicked");
+	if (inboxWord) {
+		inboxWord.addEventListener("click", () => {
+			document.cookie = "displayed=yes";
+			console.log("Inbox title clicked");
+			console.log("line53: " + document.cookie);
+			hiddenInboxBoolean();
+		});
+	};
+};
+
+const clickInboxCross = () => {
+	console.log("Inbox cross is ready");
+	if (inboxCross) {
+		inboxCross.addEventListener("click", () => {
+			console.log("close inbox button has been clicked");
+			document.cookie = "displayed=no";
+			console.log("Line 67: " + document.cookie)
+			hiddenInboxBoolean();
+		});
+	};
+};
+
+export {clickInboxCross};
+export { hiddenInboxBoolean };
+export { clickInboxTitle };
+export { removeGoogle };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,14 +5,21 @@ import "bootstrap";
 // import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
 import { initLostMapbox } from '../plugins/init_mapbox';
 import { initFoundMapbox } from '../plugins/init_mapbox';
-import { clickRadioButtons } from '../components/radioButtons.js'
-import { initAutocomplete } from '../plugins/init_autocomplete.js'
+import { clickInboxCross } from '../components/inboxButton.js';
+import { clickInboxTitle } from '../components/inboxButton.js';
+import { hiddenInboxBoolean } from '../components/inboxButton.js';
+import { removeGoogle } from '../components/inboxButton.js';
+import { clickRadioButtons } from '../components/radioButtons.js';
+import { initAutocomplete } from '../plugins/init_autocomplete.js';
+
+
+
 
 console.log("Hello world from Webpacker");
 console.log("app/javascript/packs/applications.js running");
 
-
+hiddenInboxBoolean();
+clickRadioButtons();
 initLostMapbox();
 initFoundMapbox();
 initAutocomplete();
-clickRadioButtons();
